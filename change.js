94f6b4c323c16747416096bed23eee5cce8c7f88
ChaithanyaMajety <chaithanya.majety@gmail.com>
2024-02-22 16:25:40 +0530

    Version 2 from VSCode GIT

--- change.js
+++ change.js
@@ -1,2 +1,2 @@
 console.log('This is change.js');
-console.log('version 1');
+console.log('version 2');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('This is index.js');
-console.log('version 1');
+console.log('version 2');
